a2bca772240bd0bf0f2ee72d1bd66121cf1f141f
Thelalitagarwal <77659094+Thelalitagarwal@users.noreply.github.com>
2022-08-04 01:03:15 +0530

    04_August_2022

---
+++ Complete Binary Tree.cpp
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+// A program to check if a given binary tree is complete or not
+#include<bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+    
+
+class Solution{
+public:    
+    bool isCompleteBT(Node* root){
+        queue<Node*>q;
+        q.push(root);
+        bool missing=false;
+        while(!q.empty()){
+            Node *temp=q.front();
+            q.pop();
+            if(!temp->left)
+                missing=true;
+            else 
+                if(missing) return false;
+                else  q.push(temp->left);   
+            if(!temp->right)
+                missing=true;
+            else 
+                if(missing) return false;
+                else q.push(temp->right);
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string treeString;
+		getline(cin,treeString);
+		Node* root = buildTree(treeString);
+		Solution ob;
+        if(ob.isCompleteBT(root)){
+            printf ("Complete Binary Tree\n");
+        }
+        else{
+            printf ("Not Complete Binary Tree\n");
+        } 
+    }
+    return 0;
+}
+
+
+
+
+
+// } Driver Code Ends
