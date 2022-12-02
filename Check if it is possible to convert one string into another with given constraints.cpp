79a7fda5d1a3f0629429eda349c1070a1ac715d7
Lalit Agarwal <lalitagarwal8278@gmail.com>
2022-12-02 12:07:37 +0530

    Update on 02_December_202

--- Check if it is possible to convert one string into another with given constraints.cpp
+++ Check if it is possible to convert one string into another with given constraints.cpp
@@ -1,13 +1,21 @@
 class Solution {
   public:
-    int isItPossible(string s, string t, int m, int n) {
-        string s1,s2;
-        for(int i=0;i<m;i++){
-            if(s[i]!='#') s1.push_back(s[i]);
+    int isItPossible(string S, string T, int M, int N) {
+        int i=0, j=0;
+        if(M!=N) return 0;
+        while(i<M || j<N){
+            while(S[i]=='#') i++;
+            while(T[j]=='#') j++;
+            if(i>=M && j<N) return 0;
+            if(i<M && j>=N) return 0;
+            if(S[i]!=T[j]) return 0;
+            else{
+                if(S[i]=='A' && i<j) return 0;
+                else if(S[i]=='B' && i>j) return 0;
+            }
+            i++;
+            j++;
         }
-        for(int i=0;i<n;i++){
-            if(t[i]!='#') s2.push_back(t[i]);
-        }
-        return s1==s2;
+        return 1;
     }
 };
