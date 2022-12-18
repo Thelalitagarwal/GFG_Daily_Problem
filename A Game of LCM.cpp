2a04167503b1f19ab81ec268c4a877b89e58e094
Lalit Agarwal <lalitagarwal8278@gmail.com>
2022-12-18 13:13:11 +0530

    updated on 18_December_2022

--- A Game of LCM.cpp
+++ A Game of LCM.cpp
@@ -1,19 +1,29 @@
 class Solution {
   public:
-
-    long long maxGcd(long long N) {
+    long long gcd(long long A, long long B) {
+        if (B == 0) return A;
+        return gcd(B, A % B);
+    }
+    long long lcm(int N){
+        if (N == 4) return 12;
         long long a = N * (N - 1);
         long long b = a;
-        b *= (N - 2) /__gcd(a,N-2);
-        b *= (N - 3) /__gcd(a,N-3);
-        int f = 0;
-        for (long long i = N - 2; i >= 1; i--) {
-            if (__gcd(a, i) == 1) {
+        b *= (N - 2) / gcd(b, N - 2);
+        b *= (N - 3) / gcd(b, N - 3);
+        long long c = a, f = 0;
+        for (int i = N - 2; i >= 1; i--) {
+            if (gcd(c, i) == 1) {
                 a *= i;
+                c *= i;
                 f++;
             }
             if (f == 2) break;
         }
         return max(a, b);
+    }
+    long long maxGcd(int N) {
+        if(N==4)return 12;
+        return max(lcm(N),lcm(N-1));
+        
     }
 };
