bf1f3a2ddcf922ac5e345f4906e3fb912587231e
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-01-25 10:30:12 +0530

    25_January_2023

---
+++ Type it!.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int minOperation(string s) {
+        if(s.size() <= 3)
+            return s.size();
+        int ans = INT_MAX;
+        string str = "";
+        for(int i=0; i<s.size(); i++){
+            str = str + s[i];
+            string temp = s.substr(i+1, str.size());
+            if(temp == str){
+                int ctr = str.size() + 1 + (s.size() - str.size()*2);
+                ans = min(ans, ctr);
+            }
+        }
+        return ans;
+    }
+};
