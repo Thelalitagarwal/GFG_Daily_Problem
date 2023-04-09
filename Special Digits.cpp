fdf80f0a1c40001654b49f4e4bb980fc0aa948c4
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-04-09 19:04:23 +0530

    09-Sept-2022

--- Special Digits.cpp
+++ Special Digits.cpp
@@ -1,49 +1,41 @@
 class Solution {
   public:
-    int m=1e9+7;
-    int f;
-    int fact(int x){
-        int ans=1;
-        for(int i=2;i<=x;++i)  ans=(ans*1LL*i)%m;
-        return ans;
-    }
-    int binexp(int a,int b){
-        int ans=1;
-        while(b>0){
-            if(b&1){
-                ans=(ans*1LL*a)%m;
+    bool check(int n,int c,int d) {
+        while(n) {
+            int r=n%10;
+            if(r==c || r==d) {
+                return true;
             }
-            a=(a*1LL*a)%m;
-            b=(b>>1);
+            n/=10;
         }
-        return ans;
+        return false;
     }
-    bool test(int x,int y,long long num){
-        while(num>0){
-            int rem=num%10;
-            if(rem==x || rem==y)num/=10;
-            else return 0;
+    long long binpow(long long a,long long b,long long mod) {
+        long long res=1;
+        while(b) {
+            if(b&1) {
+                res=(res*a)%mod;
+            }
+            a=(a*a)%mod;
+            b>>=1;
         }
-        return 1;
+        return res;
     }
-    int comb(int i,int n){
-       int deno=(fact(n-i)*1LL*fact(i))%m;
-       int dep= binexp(deno,m-2);
-       int ans=(f*1LL*dep)%m;
-       return ans;
-    }
-    int bestNumbers(int n, int a, int b, int c, int d) {
-        long long sum=0;
-        int x=min(a,b);
-        b=max(a,b);
-        a=x;
-        f=fact(n);
-        int ans=0;
-        for(int i=0;i<=n;++i){
-            sum=i*a+(n-i)*b;
-            if(!test(c,d,sum))continue;
-            int ct=(comb(i,n)*1LL)%m;
-            ans=(ans+0LL+ct)%m;
+    int bestNumbers(int N, int A, int B, int C, int D) {
+        if(A==B) {
+            return check(N*A,C,D);
+        } 
+        long long mod=1e9+7,ans=0;
+        vector<long long> fac(N+1,0);
+        fac[0]=1;
+        for(long long i=1;i<=N;i++) {
+            fac[i]=(fac[i-1]*i)%mod;
+        }
+        for(int i=0;i<=N;i++) {
+            int s=i*A+(N-i)*B;
+            if(check(s,C,D)) {
+                ans=(ans+(fac[N]*binpow((fac[i]*fac[N-i])%mod,mod-2,mod))%mod)%mod;
+            }
         }
         return ans;
     }
