349b6c64e310ba3545fc41099358ac10cdd14176
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-04-23 12:30:22 +0530

    23_April_2023

--- Minimum Number.cpp
+++ Minimum Number.cpp
@@ -1,12 +1,10 @@
 class Solution{
 public:
     int minimumNumber(int n,vector<int> &arr){
-        // Code here
-        int mini = INT_MAX;
+        int gcd=0;
         for(int i=0;i<n;i++){
-            if(arr[i]%2!=0) return 1;
-            mini = min(arr[i],mini);
+            gcd=__gcd(gcd,arr[i]);
         }
-     return mini;
+        return gcd;
     }
 };
