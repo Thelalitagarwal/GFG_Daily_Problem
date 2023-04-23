e3397135b5aa388e5eac8fefadf9401400f9ad16
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-04-23 12:24:56 +0530

    23_April_2023

--- Minimum Number.cpp
+++ Minimum Number.cpp
@@ -1,8 +1,12 @@
 class Solution{
 public:
     int minimumNumber(int n,vector<int> &arr){
-     for(int i=0;i<n;++i) 
-       if(arr[i] % 2 != 0)   return 1;
-     return 2;
+        // Code here
+        int mini = INT_MAX;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2!=0) return 1;
+            mini = min(arr[i],mini);
+        }
+     return mini;
     }
 };
