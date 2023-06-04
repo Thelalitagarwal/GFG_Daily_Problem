18ef89537cec12d5f79c2c477ac174609db4e074
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-06-04 08:54:25 +0530

    04-06-2023

---
+++ Reversing the equation.cpp
@@ -0,0 +1,18 @@
+class Solution{
+  public:
+    string reverseEqn (string s){
+        string res = ""; 
+        string temp = "";
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] >= '0' && s[i] <= '9')
+                temp += s[i];
+            else {
+                res = s[i] + temp + res; 
+                temp = "";
+            }
+        }
+        if(temp != "") 
+            res = temp + res;
+        return res; 
+    }
+};
