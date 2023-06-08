12a4e8c05673df687fe5efc5c9fad657fd02585d
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-06-08 09:09:58 +0530

    08-06-2023

---
+++ Find Kth permutation.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+    string kthPermutation(int n, int k){
+        string h="";
+        int chk=1;
+        for(int i=1;i<=n;i++){
+            h+=to_string(i);
+            chk*=i;
+        }
+        string ans="";
+        int i=0,g=0,r=0;
+        while(i<n){
+           r=chk/(n-i),g=k/r;
+           if(k%r==0) g--;
+           ans+=h[g];
+           h.erase(h.begin()+g);
+           k=k-(r)*(g);
+           i++;
+           chk=r;
+        }
+        return ans;
+    }
+};
