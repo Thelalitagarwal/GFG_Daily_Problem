ab2dd1e409a447fdec00433a85876fa01619849c
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-07-18 09:57:53 +0530

    18_July_2023

---
+++ Longest Repeating Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+ public:
+  int LongestRepeatingSubsequence(string str){
+    int n = str.length();
+    int dp[n+1][n+1];
+    for (int i=0; i<=n; i++)
+        for (int j=0; j<=n; j++)
+            dp[i][j] = 0;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            if (str[i-1] == str[j-1] && i != j)
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+        }
+    }
+    return dp[n][n];
+  }
+};
