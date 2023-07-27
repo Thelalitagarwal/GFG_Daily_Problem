e0b96fa9063e67edd1d9b66030ae2207d7996500
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-07-27 10:32:34 +0530

    27_July_2023

---
+++ Heap Sort.cpp
@@ -0,0 +1,33 @@
+class Solution{
+    public:
+    void heapify(int arr[], int n, int i)  {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if(l < n && arr[l] > arr[largest]){
+            largest = l;
+        }
+        if(r < n && arr[r] > arr[largest]){
+            largest = r;
+        }
+        if(largest != i){
+            swap(arr[i] , arr[largest]);
+            heapify(arr , n , largest);
+        }
+    }
+
+    void buildHeap(int arr[], int n)  { 
+        for(int i = n/2 -1 ; i  >= 0 ; i--){
+            heapify(arr , n , i);
+        }
+    }
+    
+    public:
+    void heapSort(int arr[], int n){
+        buildHeap(arr , n);
+        for(int i = n-1;i>=0;i--){
+            swap(arr[0] , arr[i]);
+            heapify(arr , i , 0);
+        }
+    }
+};
